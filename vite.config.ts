c2c560f1849448fb1a7b7e6918a8c1f66b4e0ba0
Asep Ardi <ardi.winardi91@gmail.com>
2023-07-27 09:48:15 +0700

    init config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [{ find: "@src", replacement: path.resolve(__dirname, "src") }],
+  },
+  define: {
+    "process.env": process.env,
+  },
+});
